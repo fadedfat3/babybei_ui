a5678dc4f46a7a07d28e0f36560b011b4a84b5ca
youfan <you.fan@jfsys.com>
2019-07-31 14:00:24 +0800

    ListView loading

--- lib/pages/helpInfo.dart
+++ lib/pages/helpInfo.dart
@@ -64,59 +64,59 @@ class BackgroundImageInfoState extends State<BackgroundImageInfo> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text("背景图"),
-    ),
-    body: ListView.builder(
-      itemCount: _list.length,
-      itemBuilder: (context, index) {
-        return _renderRow(context, index);
-    },
-    )
-    );
+          title: Text("背景图"),
+        ),
+        body: ListView.builder(
+          itemCount: _list.length,
+          itemBuilder: (context, index) {
+            return _renderRow(context, index);
+          },
+        ));
   }
-  Widget  _renderRow(BuildContext context, int index){
-    if(index == _list.length){
+
+  Widget _renderRow(BuildContext context, int index) {
+    if (index == _list.length) {
       load();
     }
-    if(index > _list.length){
+    if (index > _list.length) {
       return null;
     }
     return Row(
       children: <Widget>[
-        Text(_list[index].question, style: TextStyle(fontSize: 16),),
-        Text(_list[index].answer, style: TextStyle(fontSize: 14),),
+        Text(
+          _list[index].question,
+          style: TextStyle(fontSize: 16),
+        ),
+        Text(
+          _list[index].answer,
+          style: TextStyle(fontSize: 14),
+        ),
       ],
     );
   }
 
-
-
-
-
-
-
-@override
-void initState() {
-  super.initState();
-  load();
-}
-
-Future load() async {
-  if (isLoading) {
-    return null;
+  @override
+  void initState() {
+    super.initState();
+    load();
   }
-  final response =
-  await http.get('http://localhost:8001/helpInfo/$type?page=$page');
-  if (response.statusCode == 200) {
-    setState(() {
-      final list = json.decode(response.body);
-      for (var l in list) {
-        _list.add(new BgImageInfo.fromJson(l));
-      }
-      isLoading = false;
-    });
-  } else {
-    throw Exception('请检查网络链接，稍后重试！');
+
+  Future load() async {
+    if (isLoading) {
+      return null;
+    }
+    final response =
+        await http.get('http://localhost:8001/helpInfo/$type?page=$page');
+    if (response.statusCode == 200) {
+      setState(() {
+        final list = json.decode(response.body);
+        for (var l in list) {
+          _list.add(new BgImageInfo.fromJson(l));
+        }
+        isLoading = false;
+      });
+    } else {
+      throw Exception('请检查网络链接，稍后重试！');
+    }
   }
-}
 }
